88c86eb63473c7681f3a8f24e7822a9f5e70adc8
0reo27 <stopspammingme@hotmail.ph>
2018-08-14 19:08:53 +0800

    Revert "PX to EM"
    This reverts commit 9ec35a3d57e965445ac22a1a8d41cc7d6036f93a.

--- gulpfile.js
+++ gulpfile.js
@@ -6,52 +6,22 @@ var rename = require('gulp-rename');
 var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('watch', function() {
-    gulp.watch('./src/*.scss', ['build']);
+  gulp.watch('./src/*.scss', ['build']);
 });
 
 gulp.task('build', function() {
-    gulp.src('./src/*.scss')
-        .pipe(
-            sass({ outputStyle: 'compact', precision: 10 }).on(
-                'error',
-                sass.logError
-            )
-        )
-        .pipe(autoprefixer())
-        .pipe(csscomb())
-        .pipe(gulp.dest('./dist'))
-        .pipe(cleancss())
-        .pipe(
-            rename({
-                suffix: '.min'
-            })
-        )
-        .pipe(gulp.dest('./dist'));
+  gulp.src('./src/*.scss')
+    .pipe(sass({outputStyle: 'compact', precision: 10})
+      .on('error', sass.logError)
+    )
+    .pipe(autoprefixer())
+    .pipe(csscomb())
+    .pipe(gulp.dest('./dist'))
+    .pipe(cleancss())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['build']);
-
-// Convert to EM task.
-var postcss = require('gulp-postcss');
-var pxtorem = require('postcss-pxtorem');
-var replace = require('gulp-replace');
-
-gulp.task('convertToEm', function() {
-    var plugins = [
-        pxtorem({
-            replace: true,
-            propList: [`*`]
-        })
-    ];
-    return gulp
-        .src('./src/*.scss')
-        .pipe(
-            sass({ outputStyle: 'compact', precision: 10 }).on(
-                'error',
-                sass.logError
-            )
-        )
-        .pipe(postcss(plugins))
-        .pipe(replace(/rem/g, `em`))
-        .pipe(gulp.dest('srcEm'));
-});
